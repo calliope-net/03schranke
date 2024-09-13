72fd84fc8b397d1debb25d7ef33288282c4f93d7
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-13 14:48:48 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,10 +13,27 @@ function Schranke_auf () {
     Motor2 = -100
     motors.motorPower(Motor2)
 }
+let Helligkeit = 0
 let Motor2 = 0
 basic.showString("03SCHRANKE")
-let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+let o4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 pins.digitalWritePin(DigitalPin.P0, 1)
 let Schaltwert = 61
 basic.pause(500)
 Schranke_zu()
+loops.everyInterval(400, function () {
+    Helligkeit = pins.analogReadPin(AnalogPin.P1)
+    o4digit.show(Helligkeit)
+    if (Motor2 == 0 && Helligkeit > Schaltwert) {
+        Schranke_auf()
+    } else if (Motor2 < 0 && input.pinIsPressed(TouchPin.P2)) {
+        Motor_aus()
+        for (let Index = 0; Index <= 4; Index++) {
+            basic.showNumber(Index)
+            basic.pause(1000)
+        }
+        Schranke_zu()
+    } else if (Motor2 > 0 && input.pinIsPressed(TouchPin.P3)) {
+        Motor_aus()
+    }
+})
